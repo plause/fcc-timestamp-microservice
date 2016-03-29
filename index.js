e86b272358121343ff0ccac81254e73fe2d96ba0
Huazhou Wang <plause@gmail.com>
2016-03-29 21:20:38 +0800

    Implement Timestamp Microservice

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+var http = require('http');
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var isUnix = /^[1-9][0-9]*$/;
+var isNatural = new RegExp('^(' + months.join('|') + ') ([12]?[0-9]|[0-9]|3[01]), ([1-9][0-9]*)$');
+var badParameterOutput = {'unix': null, 'natural': null};
 
+function populateIfIsUnix(parameter) {
+	if (isUnix.test(parameter)) {
+		var unix = Number(parameter);
+		var date = new Date(unix * 1000);
+
+		return {
+			'unix': unix,
+			'natural': months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()
+		};
+	}
+}
+
+function populateIfIsNatural(parameter) {
+	var result = parameter.match(isNatural);
+
+	if (result) {
+		var month = months.indexOf(result[1]);
+
+		if (month !== -1) {
+			return {
+				'unix': Date.UTC(Number(result[3]), month, Number(result[2])) / 1000,
+				'natural': parameter
+			};
+		}
+	}
+}
+
+function serveTimestampMicroservice(req, res) {
+	var parameter = decodeURIComponent(req.url.substring(1));
+	var output = populateIfIsUnix(parameter) || populateIfIsNatural(parameter);
+
+	res.end(JSON.stringify(output || badParameterOutput));
+}
+
+http.createServer(serveTimestampMicroservice).listen(process.env.PORT || 5000);
